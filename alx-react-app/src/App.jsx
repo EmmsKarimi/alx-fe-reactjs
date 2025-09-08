fc6d78ff7ee8700d27bedeb6ee251d409eb741e9
Emma Karimi <emmaannkinyua@gmail.com>
2025-09-08 23:18:05 +0300

    Added Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+
+
 import WelcomeMessage from './components/WelcomeMessage';
 
 
@@ -33,8 +38,12 @@ function App() {
       </p>
 
       {/* ✅ Added your component here */}
-      <WelcomeMessage />   
-         
+      <WelcomeMessage />  
+
+      <Header />
+      <MainContent />
+      <Footer /> 
+
     </>
   )
 }
