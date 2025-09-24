e91a6380847bcc1afbabb8459c03491dd8b4832d
Emma Karimi <emmaannkinyua@gmail.com>
2025-09-24 22:39:29 +0300

    Add updateRecipe and deleteRecipe actions to recipe store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,7 +2,25 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+
+  // Add a recipe
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  // Update a recipe by id
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  // Delete a recipe by id
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
+  // Replace entire recipe list (if needed)
   setRecipes: (recipes) => set({ recipes }),
 }));
