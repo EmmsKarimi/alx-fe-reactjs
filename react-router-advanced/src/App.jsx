8a7dce0e054159a1d35e297c2c243292d9f1df82
Emma Karimi <emmaannkinyua@gmail.com>
2025-10-19 03:39:20 +0300

    Wrap App with BrowserRouter and define base routes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,16 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile";
-import ProfileDetails from "./components/ProfileDetails";
-import ProfileSettings from "./components/ProfileSettings";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/profile" element={<Profile />}>
-        <Route path="details" element={<ProfileDetails />} />
-        <Route path="settings" element={<ProfileSettings />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/*" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
